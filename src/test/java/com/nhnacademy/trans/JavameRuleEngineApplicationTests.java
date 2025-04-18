6663dd1ee37abfc4ba00c5ea1820c2a96185cd06
Moon-janggon <suzung4@gmail.com>
2025-04-18 16:51:33 +0900

    first commit

---
+++ src/test/java/com/nhnacademy/trans/JavameRuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.trans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavameRuleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
